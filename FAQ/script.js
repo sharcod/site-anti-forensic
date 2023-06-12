b52047634b10e2fed16a4e144045a610ea9e08d2
skschqhwk0209 <80950190+skschqhwk0209@users.noreply.github.com>
2023-06-13 00:05:03 +0900

    Update script.js

--- FAQ/script.js
+++ FAQ/script.js
@@ -5,3 +5,10 @@ toggles.forEach((toggle) => {
     toggle.parentNode.classList.toggle("active");
   });
 });
+
+var footer = document.createElement("footer");
+footer.id = "myFooter";
+var text = document.createTextNode("추가적인 문의는 다음 메일로 문의주세요.  test@mail.com");
+footer.appendChild(text);
+
+document.body.appendChild(footer);
